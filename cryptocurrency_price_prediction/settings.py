f3cfe7dcf1f34b761565291e4004960cd94c9805
nagalakshmi297 <nagalakshmib48@gmail.com>
2026-04-04 14:29:01 +0530

    first commit

--- cryptocurrency_price_prediction/settings.py
+++ cryptocurrency_price_prediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_&jf3mbc9rej#_wru@k=k9p_(oe!49j5+c8u!zi+*#9#zlz56b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cryptocurrency_price_prediction.urls'
@@ -118,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
