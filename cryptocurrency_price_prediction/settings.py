591ba886e94388e493f34b8eb6a37afc6b127578
nagalakshmi297 <nagalakshmib48@gmail.com>
2026-04-09 11:30:41 +0530

    final commit

--- cryptocurrency_price_prediction/settings.py
+++ cryptocurrency_price_prediction/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,10 +77,10 @@ WSGI_APPLICATION = 'cryptocurrency_price_prediction.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
