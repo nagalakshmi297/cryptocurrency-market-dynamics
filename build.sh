81e14293e7f464f5215a0cbfae86b2dabdef5ab1
nagalakshmi297 <nagalakshmib48@gmail.com>
2026-04-10 15:05:38 +0530

    setting

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 # Exit on error
 set -o errexit
 
-# Upgrade pip and critical build tools using python -m pip
-python -m pip install --upgrade pip setuptools wheel
-
-# Install dependencies with no-cache to save space
-python -m pip install -r requirements.txt --no-cache-dir
+pip install --upgrade pip
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
