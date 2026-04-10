8a06a4f0eb1b48ca882406a13108952878a65d7f
nagalakshmi297 <nagalakshmib48@gmail.com>
2026-04-10 14:46:21 +0530

    Fix build: Ensure consistent python -m pip usage and unpin tensorflow for Render environment compatibility

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Upgrade pip and critical build tools
+# Upgrade pip and critical build tools using python -m pip
 python -m pip install --upgrade pip setuptools wheel
 
 # Install dependencies with no-cache to save space
-pip install -r requirements.txt --no-cache-dir
+python -m pip install -r requirements.txt --no-cache-dir
 
 python manage.py collectstatic --no-input
 python manage.py migrate
